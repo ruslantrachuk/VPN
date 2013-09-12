b64b3bf9f26ce6ac784eb2ebdc2b4dd3483ca085
ruslantrachuk <trachuk@gmail.com>
2013-09-12 17:25:43 +0400

    modified to support ubuntu

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
-# VPN
+# VPN node 
 
-Bare Bones (PPTP) VPN Installer for CentOS
-See [http://drewsymo.com/networking/vpn/install-ptpp/](http://drewsymo.com/networking/vpn/install-ptpp/) for more information.
+PPTP VPN Installer for Ubuntu 12.04 droplet on Digital Ocean
+In addition to PPTP instatlation and configuration the script does the following
+- change ssh access policy 
+	- switch ssh to port 102
+	- create new sudoer user 
+	- disable ssh access for root user
+- install fail2ban
+
+Based on git://github.com/drewsymo/VPN.git
 
 ## Installation
 
 To get started with your own secure VPN, simply execute the following commands at your servers command-line:
 
-	$ yum install -y git
-	$ cd /opt && git clone git://github.com/drewsymo/VPN.git
-	$ cd VPN && bash vpn-setup-vanilla.sh
+	$ apt-get install git
+	$ cd /opt && git clone git://github.com/ruslantrachuk/VPN.git
+	$ cd VPN && chmod +x vpn-setup-vanilla.sh
+	$ bash vpn-setup-vanilla.sh
 
-If you're on Linode, you can simply rebuild your instance with the `PPTP VPN Installer` [stackscript](http://www.linode.com/stackscripts/view/?StackScriptID=6346).
+You can change cridentionals in vpn-setup.sh if it's needed.
+
 
 ## Author
 
-**Drew Morris**
+**Ruslan Trachuk**
 
-+ [Blog](http://drewsymo.com)
-+ [Twitter](http://twitter.com/drewsymo)
-+ [Google+](https://plus.google.com/u/0/114153589610660530694)
--- vpn-setup-vanilla.sh
+++ vpn-setup-vanilla.sh
@@ -1,58 +1,85 @@
 #!/bin/bash -x
 
 #
-# drewsymo/VPN
+# ruslantrachuk/VPN
 #
-# Installs a PPTP VPN-only system for CentOS
-#
-# @package VPN 2.0
-# @since VPN 1.0
-# @author Drew Morris
+# Installs a PPTP VPN-only system for Ubuntu
 #
 
 (
 
+SSH_USER="vpn"
+SSH_PASS="vpn"
+
 VPN_IP=`curl ipv4.icanhazip.com>/dev/null 2>&1`
 
-VPN_USER="myuser"
-VPN_PASS="mypass"
+VPN_USER="vpnuser"
+VPN_PASS="vpnuser"
 
-VPN_LOCAL="192.168.0.150"
-VPN_REMOTE="192.168.0.151-200"
+VPN_LOCAL="192.168.244.1"
+VPN_REMOTE="192.168.244.2-10"
 
-yum -y groupinstall "Development Tools"
-rpm -Uvh http://poptop.sourceforge.net/yum/stable/rhel6/pptp-release-current.noarch.rpm
-yum -y install policycoreutils policycoreutils
-yum -y install ppp pptpd
-yum -y update
-
-echo "1" > /proc/sys/net/ipv4/ip_forward
-sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
-
-sysctl -p /etc/sysctl.conf
-
+# pptd installation & configuration
+apt-get -y install pptpd
 echo "localip $VPN_LOCAL" >> /etc/pptpd.conf # Local IP address of your VPN server
 echo "remoteip $VPN_REMOTE" >> /etc/pptpd.conf # Scope for your home network
 
 echo "ms-dns 8.8.8.8" >> /etc/ppp/options.pptpd # Google DNS Primary
-echo "ms-dns 209.244.0.3" >> /etc/ppp/options.pptpd # Level3 Primary
-echo "ms-dns 208.67.222.222" >> /etc/ppp/options.pptpd # OpenDNS Primary
+echo "ms-dns 4.4.4.4" >> /etc/ppp/options.pptpd # Google DNS Primary
+
 
 echo "$VPN_USER pptpd $VPN_PASS *" >> /etc/ppp/chap-secrets
 
-service iptables start
+/etc/init.d/pptpd restart
+
+
+# ip_forward & iptables configuration
+## ip_forward
+echo "1" > /proc/sys/net/ipv4/ip_forward
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+
+## iptables
 echo "iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> /etc/rc.local
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-service iptables save
-service iptables restart
 
-service pptpd restart
-chkconfig pptpd on
+### save iptables
+iptables-save > /etc/iptables.conf
+ 
+### enables iptables on boot
+cat > /etc/network/if-pre-up.d/iptables <<END
+#!/bin/sh
+iptables-restore < /etc/iptables.conf
+END
+ 
+chmod +x /etc/network/if-pre-up.d/iptables
+cat >> /etc/ppp/ip-up <<END
+ifconfig ppp0 mtu 1400
+END
 
-echo -e '\E[37;44m'"\033[1m Installation Log: /var/log/vpn-installer.log \033[0m"
-echo -e '\E[37;44m'"\033[1m You can now connect to your VPN via your external IP ($VPN_IP)\033[0m"
+# configure SSH
+## create new SSH user to be used to access the system
+egrep "^$SSH_USER" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+	echo "$SSH_USER exists!"
+	exit 1
+else
+	pass=$(perl -e 'print crypt($ARGV[0], "password")' $SSH_PASS)
+	useradd -m -p "$pass" "$SSH_USER" 
+	[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	usermod -aG sudo "$SSH_USER"
+fi
 
-echo -e '\E[37;44m'"\033[1m Username: $VPN_USER\033[0m"
-echo -e '\E[37;44m'"\033[1m Password: $VPN_PASS\033[0m"
+
+## change ssh access policy 
+sed -i 's/Port 22/Port 102/g' /etc/ssh/sshd_config # Use port 102 instead of 22 for SSH
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config # Disable root access via SSH
+
+## prevent IP spoofing
+echo "nospoof on" >> //etc/host.conf
+
+# install fail2ban
+apt-get -y install denyhosts fail2ban
+sed -i 's/port = ssh/port = 102/g' /etc/ssh/sshd_config # configure new SSH port 102
 
 ) 2>&1 | tee /var/log/vpn-installer.log
